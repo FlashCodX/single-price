6efa535110d085c5a8a97a45206112933333a256
FlashCodX <luis.pinto.trading@gmail.com>
2022-04-17 13:26:11 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./styles/App.module.css";
 
-function App() {
+export const App = () => {
+  const {
+    container,
+    card,
+    description,
+    joinContainer,
+    info,
+    subscription,
+    about,
+    attribution,
+  } = styles;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className={container}>
+      <main className={card}>
+        <div className={joinContainer}>
+          <h1>Join our community</h1>
+          <p className={description}>
+            30-day, hassle-free money back guarantee
+          </p>
+          <p>
+            {" "}
+            Gain access to our full library of tutorials along with expert code
+            reviews. Perfect for any developers who are serious about honing
+            their skills.
+          </p>
+        </div>
+        <div className={info}>
+          <div className={subscription}>
+            <h1>Monthly Subscription</h1>
+            <div>
+              <span>$29</span> per month
+            </div>
+            <p>Full access for less than $1 a day</p>
+            <button>Sign Up</button>
+          </div>
+          <div className={about}>
+            <h1>Why Us</h1>
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer &amp; expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access to our GitHub repos</li>
+              <li> Community forum</li>
+              <li>Flashcard decks</li>
+              <li>New videos every week</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p className={attribution}>
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/luispintodev/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Luís Pinto
+          </a>
+          .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
-}
-
-export default App;
+};
